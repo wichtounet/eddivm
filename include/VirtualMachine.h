12c8506e200d32c3d336da80dda4d84b706a7ccc
Baptiste Wicht <baptistewicht@gmail.com>
2011-06-12 16:13:37 -0700

    Change license to Boost Software License 1.0

--- include/VirtualMachine.h
+++ include/VirtualMachine.h
@@ -1,3 +1,10 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2011.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
 #ifndef VIRTUAL_MACHINE_H
 #define VIRTUAL_MACHINE_H
 
